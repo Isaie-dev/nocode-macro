6b7765b8aab809ceddb0c02b844087491ff36517
Isaie-dev <isaiemerminod@gmail.com>
2026-07-21 21:05:32 +0200

    feat(header): add import/export buttons and reorder layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,12 @@ export default function Header() {
             <button className="header-buttons directory">MyMacroFolder</button>
         </div>
         <div className="button-div">
-            <button className="header-buttons save">Save</button>
+            <button className="header-buttons export">Export</button>
+            <button className="header-buttons import">Import</button>
+            <button className="header-buttons record">Record</button>
             <button className="header-buttons start">Start Macro</button>
             <button className="header-buttons stop">Stop Macro</button>
-            <button className="header-buttons record">Record</button>
+            <button className="header-buttons save">Save</button>
         </div>
         <div className="header-aside">
         <button className="header-buttons settings">Settings</button>
