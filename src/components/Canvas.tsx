bb1a87dbd13b2357d1ba71d73e3be160f55d1397
Isaie-dev <isaiemerminod@gmail.com>
2026-07-22 22:15:37 +0200

    feat(canvas): placed cart template on canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,9 +1,16 @@
 //imports
-import { useRef, MouseEvent, useState, useEffect, WheelEventHandler } from 'react';
+import { useRef, MouseEvent, useState } from 'react';
 
 export default function Canvas() {
 //script
 const [backgroundZoom, setBackgroundZoom] = useState(4.5);
+const [placedCartPositions, setPlacedCartPositions] = 
+useState(
+    {   
+        top : 400,
+        left : 700
+    }
+);
 
 const [isHolding, setIsHolding] = useState(false);
 
@@ -45,8 +52,8 @@ function scrollIfHolding(event: MouseEvent<HTMLDivElement>){
 
         setCameraCoordinates(
             {
-                x : cameraCoordinates.x + deltaX,
-                y : cameraCoordinates.y + deltaY
+                x : cameraCoordinates.x - deltaX,
+                y : cameraCoordinates.y - deltaY
             }
         )
 
@@ -57,6 +64,12 @@ function scrollIfHolding(event: MouseEvent<HTMLDivElement>){
             }
         )
 
+        setPlacedCartPositions(
+            {
+                top : placedCartPositions.top - deltaY,
+                left : placedCartPositions.left - deltaX
+            }
+        )
     }
 
 }
@@ -76,6 +89,7 @@ else {
 return (
     <div className="canvas-div">
         <div ref={canvasRef} style={{backgroundPositionX: cameraCoordinates.x + "px", backgroundPositionY: cameraCoordinates.y + "px", backgroundImage: "radial-gradient(circle,rgba(255, 255, 255, 1) "+ backgroundZoom / 10 +"vh ,rgba(212, 0, 190, 0) "+ backgroundZoom / 10 +"vh)", backgroundSize:`${backgroundZoom}vh ${backgroundZoom}vh`}} onMouseDown={wichClick} onMouseUp={isntHolding} onMouseMove={scrollIfHolding} onMouseLeave={isntHolding} onWheel={zoomUpOrDown} className="canvas">
+            <div style={{transform: `scale(${backgroundZoom})`, left: placedCartPositions.left + "px", top: placedCartPositions.top + "px"}} className="add-cart placed-cart"><p className="cart-mouse">Mouse</p><p className="info">?</p></div>
         </div>
     </div>
   );
